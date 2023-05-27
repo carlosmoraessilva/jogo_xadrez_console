5b14bca0a0581753db6e12f6005cb3e2c49c58a7
Carlos Moraes <carlosm1412@gmail.com>
2023-05-27 16:27:55 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -143,6 +143,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) ||(p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+
+                }
+            }
+
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -164,7 +180,7 @@ namespace xadrez
 
             }
 
-            Peca p = tab.peca(destino);
+           
 
             // #jogadaespecial en passant
 
